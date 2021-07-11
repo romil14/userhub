edd6ed0c7b0a2ee53048e07dfb0236c595873411
Romil <chandragade.romil@gmail.com>
2021-07-11 23:09:25 +0530

    Installed react-bootstrap, created Layout and Home component

---
+++ user-hub-spa/src/components/Layout/Layout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Row} from 'react-bootstrap';
+import Container from 'react-bootstrap/Container'
+
+const layout = (props) => {
+    return(
+        <Container >
+            <Row>
+            This is the place for the navigation component.
+            </Row>
+            <main>
+                {props.children}
+            </main>
+        </Container >
+    )
+}
+
+export default layout;
