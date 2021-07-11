9c980e8d07590974cbf9b8cf030a333b00a72775
Romil <chandragade.romil@gmail.com>
2021-07-12 01:48:48 +0530

    add menu and not fount component

--- user-hub-ng-spa/src/app/app.module.ts
+++ user-hub-ng-spa/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { RouterModule } from '@angular/router';
+import { NotFoundComponent } from './error-pages/not-found/not-found.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MenuComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+      { path: '404', component : NotFoundComponent},
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
+      { path: '**', redirectTo: '/404', pathMatch: 'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
