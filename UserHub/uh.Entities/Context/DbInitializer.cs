76ff5fb67dee4cbacf29408eb5a87f2a87883473
Romil <chandragade.romil@gmail.com>
2021-07-13 16:03:18 +0530

    Added code to initialize DB if not exixt

---
+++ UserHub/uh.Entities/Context/DbInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uh.Entities.Context
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(RepositoryContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
